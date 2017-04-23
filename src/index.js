f195a1e291836e9495c37e2799f877dc9f65357b
Zac Collier <zacacollier@gmail.com>
2017-04-23 17:57:48 -0500

    Begin merging in site files

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import './styles/index.css';
+import './styles/style.min.css';
 
 ReactDOM.render(
   <App />,
-  document.getElementById('ghwidget')
+  document.getElementById('screen')
 );
